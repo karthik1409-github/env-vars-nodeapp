cb31189d120c1d7ef139a984cf728871298ef79d
karthik1409-github <karthikeyan.jayaraman@citiustech.com>
2020-12-13 15:22:20 +0000

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,4 @@ RUN npm install
 
 EXPOSE 3000
 
-ENV MESSAGE "Hello All"
-ENV NAME "Karthikeyan. J"
-
 CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 var http = require('http');
+var fs = require('fs');
+
+
 
 var server = http.createServer(function (request, response) 
 {
+  fs.readFile('/etc/config/MESSAGE', 'UTF-8', (err, fileData) => {
+    if(err)
+    {
+      console.log(err)
+    }
+    else
+    {
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write("'MESSAGE' (from evn variable): " +process.env.MESSAGE +"<br/>");
+      response.write("'MESSAGE' (from volume): " +  fileData);
+    }
+  });
   const message = process.env.MESSAGE;
   const name = process.env.NAME;
   response.write(`Message: ${message}\n`);
