7fbabd16a117eb1dc0e882b6b317f30dccb309c0
karthik1409-github <karthikeyan.jayaraman@citiustech.com>
2020-12-13 17:37:40 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -8,20 +8,15 @@ var server = http.createServer(function (request, response)
   fs.readFile('/etc/config/MESSAGE', 'UTF-8', (err, fileData) => {
     if(err)
     {
-      console.log(err)
+        console.log(err)
     }
     else
     {
-      response.writeHead(200, {"Content-Type": "text/html"});
-      response.write("'MESSAGE' (from evn variable): " +process.env.MESSAGE +"<br/>");
-      response.write("'MESSAGE' (from volume): " +  fileData);
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("'MESSAGE' (from evn variable): " +process.env.MESSAGE +"<br/>");
+        response.write("'MESSAGE' (from volume): " +  fileData);
     }
   });
-  const message = process.env.MESSAGE;
-  const name = process.env.NAME;
-  response.write(`Message: ${message}\n`);
-  response.write(`From: ${name}\n`);
-  response.end(`\n`);
 });
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`Application listening on ${port}`));
