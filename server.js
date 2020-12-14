62a296301587566cbad4c0753b005d978897fc0b
karthik1409-github <karthikeyan.jayaraman@citiustech.com>
2020-12-14 14:47:49 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 var fs = require('fs');
-var server = http.createServer(function (request, response) 
-{
-  fs.readFile('/etc/config/MESSAGE', 'UTF-8', (err, fileData) => {
+var server = http.createServer(function (request, response) {
+  fs.readFile('/etc/config/MESSAGE', 'UTF-8', (err, fileData) => 
+  {
     if(err)
     {
       response.writeHead(200, {"Content-Type": "text/html"});
@@ -17,6 +17,7 @@ var server = http.createServer(function (request, response)
     }
     response.end();
   });
+})
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`Application listening on ${port}`));
